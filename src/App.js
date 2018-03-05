9c61c87b4e1343d60ee909c479cc3afb11a9ed50
Jason Lim <jihoon2514@gmail.com>
2018-03-05 00:01:01 -0500

    Use of API KEY

--- src/App.js
+++ src/App.js
@@ -3,15 +3,68 @@ import ViewerTemplate from './components/ViewerTemplate';
 import SpaceNavigator from './components/SpaceNavigator';
 import Viewer from './components/Viewer';
 
+import * as api from './lib/api';
+
 class App extends Component {
+  state = {
+    loading: false,
+    maxDate: null,
+    date: null,
+    urL: null,
+    mediaType: null
+  }
+
+  getAPOD = async (date) => {
+    if (this.state.loading) return;
+
+    // Loading state start
+    this.setState({
+      loading: true
+    });
+
+    try {
+      const response = await api.getAPOD(date);
+      const { date: retrievedDate, url, media_type: mediaType } = response.data;
+      console.log(response.data);
+      console.log(response);
+      if(!this.state.maxDate) {
+        this.setState({
+          maxDate: retrievedDate
+        })
+      }
+
+      this.setState({
+        date: retrievedDate,
+        mediaType,
+        url
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
+      // Loading state finished
+      this.setState({
+        loading: false
+      });
+  }
+
+  componentDidMount() {
+    console.log("cdm");
+    this.getAPOD();
+  }
+  
+
   render() {
+    const{ url, mediaType, loading } = this.state;
+
     return (
       <ViewerTemplate
         spaceNavigator={<SpaceNavigator/>}
         viewer={(
           <Viewer 
-            url="https://www.youtube.com/embed/uj3Lq7Gu94Y?rel=0" 
-            mediaType="video"/>
+            url={url}
+            mediaType={mediaType}
+            loading={loading}/>
         )}
       />
     );
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export getAPOD = (date = '') => {
-  axios.get('https://api.nasa.gov/planetary/apod?api_key=3bTPz17XLGJErMX6uI1m1mrxBlCEDl0HN6GETyyk&date=${date}')
+export function getAPOD(date = '') {
+  return axios.get(`https://api.nasa.gov/planetary/apod?api_key=3bTPz17XLGJErMX6uI1m1mrxBlCEDl0HN6GETyyk&date=${date}`);
 }
