f7f72a7d1fdfa36b2f136f75f353d89c87c28ad4
Jason Lim <jihoon2514@gmail.com>
2018-03-06 21:19:27 -0500

    Display prev/next image

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ViewerTemplate from './components/ViewerTemplate';
 import SpaceNavigator from './components/SpaceNavigator';
 import Viewer from './components/Viewer';
-
+import moment from 'moment';
 import * as api from './lib/api';
 
 class App extends Component {
@@ -15,6 +15,7 @@ class App extends Component {
   }
 
   getAPOD = async (date) => {
+    // if it's aready requesting, then ignore
     if (this.state.loading) return;
 
     // Loading state start
@@ -24,21 +25,24 @@ class App extends Component {
 
     try {
       const response = await api.getAPOD(date);
-      const { date: retrievedDate, url, media_type: mediaType } = response.data;
-      console.log(response.data);
       console.log(response);
+      const { date: retrievedDate, url, media_type: mediaType } = response.data;
+  
+      // if there is no maxDate, then set maxDate as retrieved date
       if(!this.state.maxDate) {
         this.setState({
           maxDate: retrievedDate
         })
       }
 
+      // set State as received data
       this.setState({
         date: retrievedDate,
         mediaType,
         url
       });
     } catch (e) {
+      // error?
       console.log(e);
     }
 
@@ -46,6 +50,20 @@ class App extends Component {
       this.setState({
         loading: false
       });
+  }
+
+  handlePrev = () => {
+    const { date } = this.state;
+    const prevDate = moment(date).subtract(1, 'days').format('YYYY-MM-DD');
+    console.log(prevDate);
+    this.getAPOD(prevDate);
+  }
+
+  handleNext = () => {
+    const { date } = this.state;
+    const nextDate = moment(date).add(1, 'days').format('YYYY-MM-DD');
+    console.log(nextDate);
+    this.getAPOD(nextDate);
   }
 
   componentDidMount() {
@@ -56,15 +74,21 @@ class App extends Component {
 
   render() {
     const{ url, mediaType, loading } = this.state;
+    const { handlePrev, handleNext } = this;
 
     return (
       <ViewerTemplate
-        spaceNavigator={<SpaceNavigator/>}
+        spaceNavigator={
+          <SpaceNavigator
+            onPrev={handlePrev}
+            onNext={handleNext}
+          />}
         viewer={(
           <Viewer 
             url={url}
             mediaType={mediaType}
-            loading={loading}/>
+            loading={loading}
+          />
         )}
       />
     );
